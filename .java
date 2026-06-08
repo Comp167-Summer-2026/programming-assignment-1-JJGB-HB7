57788d4a654b5cb691b0909e05b6fa4bc7f040c3
Julius <julesjohn3h@gmail.com>
2026-06-07 23:26:16 -0400

    ConvertTemperature method created

--- .java
+++ .java
@@ -2,5 +2,12 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
 
-    public static double convertTemperature(double temperature, String unit)
+    public static double convertTemperature(double temperature, String unit){
+        if (unit.equalsIgnoreCase("C")){
+            return (temperature * 9.0 / 5.0) + 32;
+        }
+        else {
+            return (temperature - 32) * 5.0 / 9.0;
+        }
+    }
 }
